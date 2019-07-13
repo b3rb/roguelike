3d5c7a3855b719c8a651ddce84741bf52a6c23e9
b e r b <b.french@nyharborstudent.org>
2019-07-13 19:07:58 -0400

    reorganized file structure

--- src/player.c
+++ src/player.c
@@ -1,4 +1,46 @@
 #include "roguelike.h"
 #include "player.h"
 
-#include "player/movement.c"
+void PrintPlayer(int x, int y);
+bool MovePlayer(int direction, char map[255][255]);
+
+void PrintPlayer(int x, int y) {
+
+	/* moves cursor to spawn position and places player */
+	mvprintw(y, x, "@");
+	move(y, x);
+}
+
+bool MovePlayer(int direction, char map[255][255]) {
+
+bool success = true;;
+int x, y;
+getyx(stdscr, y, x); /* gets player coords */
+
+	/* detects if movement is possible */
+	switch(direction) {
+		case UP:
+			if (map[y - 1][x] == FLOOR || map[y - 1][x] == HALL) --y;
+			break;
+
+		case DOWN:
+			if (map[y + 1][x] == FLOOR || map[y + 1][x] == HALL) ++y;
+			break;
+
+		case LEFT:
+			if (map[y][x - 1] == FLOOR || map[y][x - 1] == HALL) --x;
+			break;
+
+		case RIGHT:
+			if (map[y][x + 1] == FLOOR || map[y][x + 1] == HALL) ++x;
+			break;
+
+		default:
+			break;
+	}
+	PrintPlayer(x, y);
+
+return success;
+}
+
+
